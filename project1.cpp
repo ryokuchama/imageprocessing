3c7b2aa2573999c74488b62b03d756e8c15f64a2
tarou <r.madrid.pg@gmail.com>
2020-10-12 00:37:56 +0900

    1011

--- project1.cpp
+++ project1.cpp
@@ -1,11 +1,8 @@
-#include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
 
 int main() {
-    cv::Mat img
-    img = imread("imageprocessing/dog.png");
 
-    cv::imshow("title", img);
-    cv::waitkey(0);
+     std::cout << "Hello! World!" << std::endl;
 
     return 0;
 }
