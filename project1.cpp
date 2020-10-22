79012b7845c079aeff7273035c1728d9fc79ca80
tarou <r.madrid.pg@gmail.com>
2020-10-23 00:32:03 +0900

    1022

--- project1.cpp
+++ project1.cpp
@@ -1,8 +1,20 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main() {
+using namespace std;
 
-     std::cout << "Hello! World!" << std::endl;
+int main() {
+    cout << "start" << endl;
+
+    cv::Mat img = cv::imread("./images/dog.jpg");
+    if(img.empty()) return -1;
+
+    cv::namedWindow("title", cv::WINDOW_AUTOSIZE);
+    cv::imshow("title", img);
+    cv::waitKey(0);
+
+    cv::destroyAllWindows();
+    cout << "Finnish" << endl;
 
     return 0;
 }
