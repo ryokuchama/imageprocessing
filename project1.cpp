c2dec279ec4c03eafe89458bee3b0bc0fd856ff0
tarou <r.madrid.pg@gmail.com>
2020-10-25 01:18:17 +0900

    1024

--- project1.cpp
+++ project1.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main() {
     cout << "start" << endl;
 
+    int red, green, blue;
+
     cv::Mat img = cv::imread("./images/dog.jpg");
     if(img.empty()) return -1;
 
     cv::namedWindow("title", cv::WINDOW_AUTOSIZE);
     cv::imshow("title", img);
+    cv::Scalar(blue, red, green);
     cv::waitKey(0);
 
     cv::destroyAllWindows();
