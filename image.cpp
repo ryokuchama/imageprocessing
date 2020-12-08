d222c1eba552ae0869bda5cb22b57a70088f0c8b
tarou <r.madrid.pg@gmail.com>
2020-12-09 00:27:13 +0900

    1209

--- image.cpp
+++ image.cpp
@@ -7,13 +7,27 @@ using namespace cv;
 int main() {
     Mat img = imread("./images/dog.jpg", 0);
     Mat channels[3];
-    Mat bin_img;
+    Mat result;
+    double kernel[3][3];
+
+    Point anchor = Point(-1, 1);
+    double delta = 0;
+    int ddepth = -1;
 
     if (img.empty()) return -1;
 
-    split(img, channels);
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> kernel[i][j];
+        }
+    }
 
-    threshold(img, bin_img, 0, 255, THRESH_OTSU);
+    filter2D(
+        img, result, ddepth, Mat(3, 3, CV_64F, &kernel), anchor, delta, BORDER_DEFAULT
+        );
+
+    imshow("filter2D", result);
+    waitKey(0);
 
     return 0;
 }
