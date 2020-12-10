bb87b9b52218b068f12193c5674d2d5cb36c1fa6
tarou <r.madrid.pg@gmail.com>
2020-12-11 00:39:56 +0900

    1211

--- image.cpp
+++ image.cpp
@@ -4,30 +4,33 @@
 using namespace std;
 using namespace cv;
 
-int main() {
-    Mat img = imread("./images/dog.jpg", 0);
-    Mat channels[3];
-    Mat result;
-    double kernel[3][3];
+class ImageProcessing {
 
-    Point anchor = Point(-1, 1);
-    double delta = 0;
-    int ddepth = -1;
+    int main() {
+        Mat img = imread("./images/dog.jpg", 0);
+        Mat channels[3];
+        Mat result;
+        double kernel[3][3];
 
-    if (img.empty()) return -1;
+        Point anchor = Point(-1, 1);
+        double delta = 0;
+        int ddepth = -1;
 
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> kernel[i][j];
+        if (img.empty()) return -1;
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                cin >> kernel[i][j];
+            }
         }
+
+        filter2D(
+            img, result, ddepth, Mat(3, 3, CV_64F, &kernel), anchor, delta, BORDER_DEFAULT
+            );
+
+        imshow("filter2D", result);
+        waitKey(0);
+
+        return 0;
     }
-
-    filter2D(
-        img, result, ddepth, Mat(3, 3, CV_64F, &kernel), anchor, delta, BORDER_DEFAULT
-        );
-
-    imshow("filter2D", result);
-    waitKey(0);
-
-    return 0;
-}
+};
