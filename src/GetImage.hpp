5147007b59e92e681dc6ccb3105c56bcf4b9a13f
tarou <r.madrid.pg@gmail.com>
2021-01-10 00:30:46 +0900

    0

--- src/GetImage.hpp
+++ src/GetImage.hpp
@@ -22,62 +22,27 @@ class GetImage {
 
         Mat img;
         Mat gray;
-        Mat bin;
-        Mat reverse;
 
         img = getImage(s);
-        
+
+        Mat input(img.rows, img.cols, img.type());
         cvtColor(img, gray, COLOR_BGR2GRAY);
-        adaptiveThreshold(gray, bin, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 8);
-        bin = ~bin;
+        adaptiveThreshold(gray, input, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 8);
 
         vector<vector<Point>> contours;
-        vector<Vec4i> hierarchy;
 
-        findContours(bin, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        findContours(input, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-        for(int i = 0; i < contours.size(); ++i) {
-            Point min = minPoint(contours.at(i));
-            Point max = maxPoint(contours.at(i));
+        auto maxContour = max_element(
+            contours.begin(), contours.end(), [](vector<Point> x, vector<Point> y
+            ){
+                return contourArea(x) < contourArea(y);
+        });
 
-            Rect rect(min, max);
-
-            rectangle(img, rect, Scalar(0, 255, 0), 2, 8);
-        }
-    }
-
-    // max coordinate
-    private: Point minPoint(vector<Point> contours) {
-
-        double minX;
-        double minY;
-
-        for(int i = 1; i , contours.size(); ++i) {
-            if (minX > contours.at(i).x) {
-                minX = contours.at(i).x;
-            }
-            if (minY > contours.at(i).y) {
-                minY = contours.at(i).y;
-            }
-
-            return Point(minX, minY);
-        }
-    }
-
-    private: Point maxPoint(vector<Point> contours) {
-
-        double maxX;
-        double maxY;
-
-        for(int i = 1; i < contours.size(); ++i) {
-            if(maxX < contours.at(i).x) {
-                maxX = contours.at(i).x;
-            }
-            if(maxY < contours.at(i).y) {
-                maxY = contours.at(i).y;
-            }
-        }
-
-        return Point(maxX, maxY);
+        auto area = [maxContour](){
+            Mat pointf;
+            Mat(*maxContour).convertTo(pointf, CV_32F);
+            return fitEllipse(pointf);
+        }();
     }
 };
