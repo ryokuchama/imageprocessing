c1a7dadbd770ad48478d79481b631ce28559fd00
tarou <r.madrid.pg@gmail.com>
2021-01-15 00:45:12 +0900

    0115

---
+++ build/CMakeFiles/imageprocessing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageprocessing.dir/main.cpp.o"
+  "imageprocessing"
+  "imageprocessing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageprocessing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kajikenshou/imageprocessing
+# Build directory: /Users/kajikenshou/imageprocessing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
