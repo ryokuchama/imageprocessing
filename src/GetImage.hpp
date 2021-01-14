1a9270e3a34690ea69f62502046ba6d1f834f075
tarou <r.madrid.pg@gmail.com>
2021-01-15 00:13:29 +0900

    0115

--- src/GetImage.hpp
+++ src/GetImage.hpp
@@ -33,7 +33,7 @@ class GetImage {
         for (int i = 0; i < contours.size(); ++i) {
             double area = contourArea(contours[i], false);
 
-            if (area > result) {
+            if (area > 10000) {
                 vector<Point> approx;
                 approxPolyDP(
                     Mat(contours[i]), approx, 0.01 * arcLength(contours[i], true), true
@@ -41,9 +41,9 @@ class GetImage {
                 if (approx.size() == 4) {
                     drawContours(img, out, i, Scalar(255, 0, 0, 255), 3, LINE_AA, hierarchy, maxLevel);
 
-                    if (out != NULL) {
-                        imwrite("../images/test.png", out);
-                    }                   
+                    imshow("out", out);
+                    waitKey(0);
+                    destroyAllWindows();                   
                 }
             }
         }     
